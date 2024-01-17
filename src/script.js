a465d91fd54e669a0938f056596461492c5ce149
roseann1024 <156710202+roseann1024@users.noreply.github.com>
2024-01-16 18:21:23 -0700

    injected the current weather details

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#weather-app-temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
 function searchCity(city) {
