f703f5ceb442f7ee5a23c502d2cfc574204cb143
roseann1024 <156710202+roseann1024@users.noreply.github.com>
2024-01-16 17:29:48 -0700

    added min-height for background

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
+function displayTemp(response) {
+  c
+}
+
+function searchCity(city) {
+  let apiKey = "f78a0dbafabf13190c441b8cod34ftff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
 function searchButton(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input-city");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let formInput = document.querySelector("#form-input");
