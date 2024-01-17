aa6c41c5b98c8ecd177b9b711d861d876e18b48e
roseann1024 <156710202+roseann1024@users.noreply.github.com>
2024-01-16 19:07:48 -0700

    updated the minutes

--- src/script.js
+++ src/script.js
@@ -29,9 +29,9 @@ function formatDate(date) {
   ];
 
   let day = days[date.getDay()];
-  // if (minutes < 10) {
-  // minutes = `0${minutes}`;
-  // }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return `${day} ${hours}:${minutes}`;
 }
 
