e57262e27381c781f1e5fcf3250f5219c4e6c493
roseann1024 <156710202+roseann1024@users.noreply.github.com>
2024-01-17 19:36:15 -0700

    Redesign the CSS

--- src/script.js
+++ src/script.js
@@ -5,19 +5,19 @@ function displayTemp(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#weather-app-icon");
+  let now = new Date(response.data.time * 1000);
 
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = formatDate(date);
+  timeElement.innerHTML = formatDate(now);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
 
-function formatDate(date) {
+function formatDate(now) {
   let days = [
     "Sunday",
     "Monday",
@@ -28,15 +28,33 @@ function formatDate(date) {
     "Saturday",
   ];
 
-  let day = days[date.getDay()];
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
+  let day = days[now.getDay()];
+  let minutes = now.getMinutes();
+  let hours = now.getHours();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+  let dates = now.getDate();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-  return `${day} ${hours}:${minutes}`;
+  return `${hours}:${minutes}, ${day}
+  (${month} ${dates}, ${year})`;
 }
 function searchCity(city) {
   let apiKey = "f78a0dbafabf13190c441b8cod34ftff";
